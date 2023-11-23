faec6dd8e791f770aa79dd22af54376ee8fef8b5
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-22 22:56:38 -0500

    Add ability to send SOL from connected phantom wallet to an input public key on devnet.

--- frontend/components/SendSolForm.tsx
+++ frontend/components/SendSolForm.tsx
@@ -1,13 +1,44 @@
 import { useState } from "react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
 import styles from "../styles/Home.module.css";
 
 export default function SendSolForm() {
-  const [amount, setAmount] = useState("");
+  const [sol, setSol] = useState("");
   const [recipient, setRecipient] = useState("");
+
+  // Connect to wallet
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
 
   const sendSol = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log(`Send ${amount} SOL to ${recipient}`);
+
+    // Check if wallet is connected
+    if (!connection || !publicKey) {
+      alert("Connection not found");
+      return;
+    }
+
+    // Create transaction and transfer instruction
+    const transaction = new web3.Transaction();
+    const recipientPublicKey = new web3.PublicKey(recipient);
+    const lamports = web3.LAMPORTS_PER_SOL * parseFloat(sol);
+
+    const transferSolInstruction = web3.SystemProgram.transfer({
+      fromPubkey: publicKey,
+      toPubkey: recipientPublicKey,
+      lamports,
+    });
+
+    transaction.add(transferSolInstruction);
+
+    // Send transaction and display confirmation
+    sendTransaction(transaction, connection).then((signature) => {
+      console.log(`Transaction sent: ${signature}`);
+    });
+
+    console.log(`Send ${sol} SOL to ${recipient}`);
   };
 
   return (
@@ -20,7 +51,7 @@ export default function SendSolForm() {
           className={styles.formField}
           placeholder="e.g. 0.1"
           required
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={(e) => setSol(e.target.value)}
         />
         <br />
         <label htmlFor="recipient">Send SOL to:</label>

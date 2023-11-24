422797a7c4fab5f60f1680c72d5fec9fd4d3e20b
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-23 23:00:23 -0500

    Redirect user to dashboard when they connect their wallet.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,31 @@
 "use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useWallet } from '@solana/wallet-adapter-react';
 import styles from "../styles/Home.module.css";
-import { NavBar } from "../components/NavBar";
 import Head from "next/head";
-import Link from "next/link";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function Home() {
+  const { connected } = useWallet();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (connected) {
+      router.push('/dashboard');
+    }
+  }, [connected, router]);
+
   return (
     <div className={styles.App}>
       <Head>
         <title>Funder</title>
         <meta name="description" content="Funder" />
       </Head>
-      <NavBar />
       <div className={styles.AppBody}>
         <h1>Welcome to Funder</h1>
         <h2>Connect your wallet to continue</h2>
-        <Link href="/dashboard">Dashboard</Link>
+        <WalletMultiButton />
       </div>
     </div>
   );
